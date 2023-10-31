00877578895d3154a507ef24de6944356e6496c0
parince <konrad.tupta@gmail.com>
2023-10-31 20:58:09 +0100

    Initial commit

---
+++ src/main/java/com/battlemetrics/BattleMetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.battlemetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleMetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleMetricsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battlemetrics/BattleMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battlemetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleMetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
