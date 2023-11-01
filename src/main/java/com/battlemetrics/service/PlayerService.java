2652d924f7bf05cbed3713af10b90a9cd8c695e4
parince <konrad.tupta@gmail.com>
2023-11-01 11:26:46 +0100

    Created getPlayerSessionsById inside player service

--- src/main/java/com/battlemetrics/service/PlayerService.java
+++ src/main/java/com/battlemetrics/service/PlayerService.java
@@ -2,6 +2,7 @@ package com.battlemetrics.service;
 
 import com.battlemetrics.Constants;
 import com.battlemetrics.model.response.Player;
+import com.battlemetrics.model.response.PlayerSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,5 +13,10 @@ public class PlayerService {
     public Player getPlayerById(String playerId) {
         String apiUrl = Constants.API_URL + "/players/" + playerId;
         return new RestTemplate().getForObject(apiUrl, Player.class);
+    }
+
+    public PlayerSession getPlayerSessionsById(String playerId) {
+        String apiUrl = Constants.API_URL + "/players/" + playerId + "/relationships" +"/sessions";
+        return new RestTemplate().getForObject(apiUrl, PlayerSession.class);
     }
 }
