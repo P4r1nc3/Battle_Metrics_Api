f2d55512fd7e8df29333d6a014e5ba9c7b141812
parince <konrad.tupta@gmail.com>
2023-11-01 13:03:59 +0100

    Created player online status response model

---
+++ src/main/java/com/battlemetrics/model/response/PlayerOnlineStatus.java
@@ -0,0 +1,12 @@
+package com.battlemetrics.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerOnlineStatus {
+    private boolean online;
+}
