e55a773b02736a52e34168067f633c9997830a94
parince <konrad.tupta@gmail.com>
2023-11-05 15:23:06 +0100

    Remade tracker service

--- src/main/java/com/battlemetrics/service/TrackerService.java
+++ src/main/java/com/battlemetrics/service/TrackerService.java
@@ -1,36 +1,34 @@
 package com.battlemetrics.service;
 
+import com.battlemetrics.model.dao.TrackedPlayer;
 import com.battlemetrics.model.response.PlayerStatusResponse;
+import com.battlemetrics.repository.TrackedPlayerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TrackerService {
     private final PlayerService playerService;
-    private final Map<String, PlayerStatusResponse> trackedPlayers = new HashMap<>();
+    private final TrackedPlayerRepository trackedPlayerRepository;
 
     public void addPlayerToTracking(String playerId) {
         PlayerStatusResponse status = playerService.isPlayerOnline(playerId);
-        trackedPlayers.put(playerId, status);
+
+        TrackedPlayer trackedPlayer = new TrackedPlayer();
+        trackedPlayer.setPlayerId(playerId);
+        trackedPlayer.setOnline(status.isOnline());
+
+        trackedPlayerRepository.save(trackedPlayer);
     }
 
     public void removePlayerFromTracking(String playerId) {
-        trackedPlayers.remove(playerId);
+        trackedPlayerRepository.deleteById(playerId);
     }
 
-    public Map<String, PlayerStatusResponse> getTrackedPlayers() {
-        return new HashMap<>(trackedPlayers);
-    }
-
-    public PlayerStatusResponse getLastStatus(String playerId) {
-        return trackedPlayers.getOrDefault(playerId, new PlayerStatusResponse());
-    }
-
-    public void setLastStatus(String playerId, PlayerStatusResponse status) {
-        trackedPlayers.put(playerId, status);
+    public List<TrackedPlayer> getTrackedPlayers() {
+        return  trackedPlayerRepository.findAll();
     }
 }
