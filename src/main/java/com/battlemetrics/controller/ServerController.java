d70097477e7e1762346a16aca693125058d248d8
parince <konrad.tupta@gmail.com>
2023-11-30 20:26:10 +0100

    Created new endpoint inside ServerController.java

--- src/main/java/com/battlemetrics/controller/ServerController.java
+++ src/main/java/com/battlemetrics/controller/ServerController.java
@@ -1,18 +1,28 @@
 package com.battlemetrics.controller;
 
 import com.battlemetrics.dao.response.bmapi.ServerResponse;
+import com.battlemetrics.model.Player;
 import com.battlemetrics.service.ServerService;
+import com.battlemetrics.service.TeamDetectorService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/servers")
 public class ServerController {
-    private ServerService serverService;
+    private final ServerService serverService;
+    private final TeamDetectorService teamDetectorService;
 
     @GetMapping("/{serverId}")
     public ServerResponse getServerById(@PathVariable String serverId, @RequestParam(required = false) String include) {
         return serverService.getServerById(serverId, include);
+    }
+
+    @GetMapping("/{serverId}/players")
+    public List<Player> getPlayersList(@PathVariable String serverId) {
+        return teamDetectorService.getPlayersList(serverId);
     }
 }
