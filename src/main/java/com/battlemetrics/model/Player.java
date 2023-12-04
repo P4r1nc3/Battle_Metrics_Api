6f6dec8a5a696f105395ba155193a9b896d143c3
parince <konrad.tupta@gmail.com>
2023-12-04 22:59:38 +0100

    Remade Player.java

--- src/main/java/com/battlemetrics/model/Player.java
+++ src/main/java/com/battlemetrics/model/Player.java
@@ -1,12 +1,30 @@
 package com.battlemetrics.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Builder
+@ToString
 @AllArgsConstructor
 public class Player {
     private String nick;
+    private String steamId;
+    private String steamUrl;
     private String battleMetricsId;
-    private String profileUrl;
+    private String battleMetricsUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(nick, player.nick);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nick);
+    }
 }
