f8dfee66a914181d5b4f818668a169ffe8c0c297
parince <konrad.tupta@gmail.com>
2023-12-05 22:25:16 +0100

    Clarified variable name

--- src/main/java/com/battlemetrics/Constants.java
+++ src/main/java/com/battlemetrics/Constants.java
@@ -1,6 +1,6 @@
 package com.battlemetrics;
 
 public class Constants {
-    public static final String API_URL = "https://api.battlemetrics.com";
+    public static final String BM_API_URL = "https://api.battlemetrics.com";
     public static final String STEAM_API_URL = "http://api.steampowered.com";
 }
